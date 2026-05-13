dfc9ff91837fa14777451fd84bd7226c192966b4
nickvirus11-boop <nickvirus11@gmail.com>
2026-05-13 18:11:38 +0930

    Initialize tkinter app for SpotDifferenceGame

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-gg
+import tkinter as tk
+from game import SpotDifferenceGame
+
+
+def main():
+    root = tk.Tk()
+    SpotDifferenceGame(root)
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()gg
