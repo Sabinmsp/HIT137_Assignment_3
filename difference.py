0c2a0a44783817e3b97392e42e6d46eea3bbd157
Karanpandeyjpeg <pandeykaran321@gmail.com>
2026-05-15 20:13:11 +0930

    Delete difference.py

--- difference.py
+++
@@ -1,7 +0,0 @@
-class Difference:
-    def __init__(self, x, y, radius, diff_type):
-        self.x = x
-        self.y = y
-        self.radius = radius
-        self.diff_type = diff_type
-        self.found = False
